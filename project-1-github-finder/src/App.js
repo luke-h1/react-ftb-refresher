0eda430d8d8dbf5ee47ade1ec7265a6a0cad6781
luke-h1 <luke.howsam@yahoo.com>
2020-10-26 21:35:41 +0000

    update

--- project-1-github-finder/src/App.js
+++ project-1-github-finder/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const name = 'Luke';
+    const loading = true;
+    // if (loading) {
+    //   return <h1>Loading</h1>;
+    // }
+    return (
+      <div className='App'>
+        {loading ? <h1>Loading</h1> : <h1>hello {name}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
+// on component, state & props lession (very start)
