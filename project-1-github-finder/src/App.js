d0f4ea645100502376802a258c9b6a780e832f6e
luke-h1 <luke.howsam@yahoo.com>
2020-10-27 18:09:38 +0000

    HTTP reqs & updating state

--- project-1-github-finder/src/App.js
+++ project-1-github-finder/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar/Navbar";
 import Users from "./components/users/Users/Users";
+import axios from 'axios';
 class App extends React.Component {
+   async componentDidMount (){
+    const BASE_URL = `https://api.github.com/users`
+    const res = await axios.get(BASE_URL); 
+    console.log(res.data)
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
-
         <div className="container">
           <Users />
         </div>
@@ -15,5 +20,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
