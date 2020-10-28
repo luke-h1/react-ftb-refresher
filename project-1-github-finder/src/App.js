be400540b7e75fc96f79f3eca8fe4dca33c95249
luke-h1 <luke.howsam@yahoo.com>
2020-10-28 13:46:40 +0000

    fetch users from api

--- project-1-github-finder/src/App.js
+++ project-1-github-finder/src/App.js
@@ -2,22 +2,30 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar/Navbar";
 import Users from "./components/users/Users/Users";
-import axios from 'axios';
+import axios from "axios";
 class App extends React.Component {
-   async componentDidMount (){
-    const BASE_URL = `https://api.github.com/users`
-    const res = await axios.get(BASE_URL); 
-    console.log(res.data)
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const BASE_URL = `https://api.github.com/users`;
+    const res = await axios.get(BASE_URL);
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
   }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
   }
 }
+// on lesson spinner component and refactoring
 export default App;
