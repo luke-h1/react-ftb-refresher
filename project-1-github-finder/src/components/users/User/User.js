5d88def307522c49bb9bceda5d2096506be1475b
luke-h1 <luke.howsam@yahoo.com>
2020-11-06 13:48:59 +0000

    updates

--- project-1-github-finder/src/components/users/User/User.js
+++ project-1-github-finder/src/components/users/User/User.js
@@ -83,15 +83,17 @@ export class User extends React.Component {
                   </>
                 )}
               </li>
-              <li>
-                {blog && (
-                  <>
-                    Website: {blog}
-                  </>
-                )}
-              </li>
+              <li>{blog && <>Website: {blog}</>}</li>
             </ul>
           </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-danger">
+            Public Repositories: {public_repos}
+          </div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
       </>
     );
