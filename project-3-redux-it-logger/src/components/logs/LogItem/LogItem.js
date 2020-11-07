2aeef63b3b5a985ad2eeede3a156771ed34ea119
luke-h1 <luke.howsam@yahoo.com>
2020-11-07 21:27:18 +0000

    avoid using moment.js

--- project-3-redux-it-logger/src/components/logs/LogItem/LogItem.js
+++ project-3-redux-it-logger/src/components/logs/LogItem/LogItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+
 const LogItem = ({ log }) => {
   return (
     <>
       <li className="collection-item">
         <div>
-          <a href="#nothing">{log.message}</a>
+          <a
+            href="#edit-log-modal"
+            className={`modal-trigger ${
+              log.attention ? 'red-text' : 'blue-text'
+            }`}
+          >
+            {log.message}
+          </a>
+          <br />
+          <span className="gray-text">
+            <span className="black-text">ID: {log.id} Last Updated By </span>
+            <span className="black-text">{log.tech}</span> on{' '} {log.date.toLocaleString()}
+          </span>
         </div>
       </li>
     </>
