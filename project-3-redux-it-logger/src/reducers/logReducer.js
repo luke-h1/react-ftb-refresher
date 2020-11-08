222415c9f8bb3a31e03aa216cfe1742107f7208d
luke-h1 <luke.howsam@yahoo.com>
2020-11-08 20:02:45 +0000

    del logs

--- project-3-redux-it-logger/src/reducers/logReducer.js
+++ project-3-redux-it-logger/src/reducers/logReducer.js
@@ -67,6 +67,9 @@ export default (state = initialState, action) => {
     case UPDATE_LOG:
       return {
         ...state,
+        logs: state.logs.map((log) =>
+          log.id === action.payload.id ? action.payload : log
+        ),
       };
   }
 };
