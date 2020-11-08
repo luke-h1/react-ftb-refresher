16c5e322e2bee96e710b39e1fb0e7d47d244f230
luke-h1 <luke.howsam@yahoo.com>
2020-11-08 15:48:21 +0000

    start to implement redux

--- project-3-redux-it-logger/src/reducers/logReducer.js
+++ project-3-redux-it-logger/src/reducers/logReducer.js
@@ -1,10 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 const initialState = {
   logs: null,
   current: null,
   loading: false,
-  error: null
+  error: null,
 };
-export default (state, action) => {
+export default (state=initialState, action) => {
   switch (action.type) {
     default:
       return state;
