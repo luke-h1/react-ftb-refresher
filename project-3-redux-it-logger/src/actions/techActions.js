0131c7541e929446b6c1c377b1f0f6b0941aeb52
luke-h1 <luke.howsam@yahoo.com>
2020-11-09 08:32:39 +0000

    techsReducer

---
+++ project-3-redux-it-logger/src/actions/techActions.js
@@ -0,0 +1,32 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR,
+} from './types';
+
+// get techs from server
+export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('http://localhost:5000/techs');
+    const data = await res.json();
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: e.response.data,
+    });
+  }
+};
+
+// set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
