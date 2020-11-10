ce6ea69d4b4fdc24f607a9e5cdc4bba3ee631955
luke-h1 <luke.howsam@yahoo.com>
2020-11-10 13:38:56 +0000

    change msg + create .gitignore

--- project-2-contact-keeper/server.js
+++ project-2-contact-keeper/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the contact keeper API ! 📱 😀');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
