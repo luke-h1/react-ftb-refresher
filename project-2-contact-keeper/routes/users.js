ce97bcf7984c047e5c400660bc4d6b684f3218e9
luke-h1 <luke.howsam@yahoo.com>
2020-11-11 20:51:29 +0000

    add user model validation

--- project-2-contact-keeper/routes/users.js
+++ project-2-contact-keeper/routes/users.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const router = require('express').Router(); 
+const router = require('express').Router();
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator/check');
 
 /* 
 @route    POST    /api/users
 @desc     Register a user 
 @access   Public
 */
-router.post('/', (req, res) => {
-  res.send('Registers a user');
-});
-
-
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required / enter a valid email').isEmail(),
+    check('password', 'Enter a password with 6 or more characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('Passed validation');
+  }
+);
 
 module.exports = router;
--- project-2-contact-keeper/server.js
+++ project-2-contact-keeper/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 
+
 // connect to mongodb
 connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the contact keeper API ! 📱 😀');
@@ -14,7 +18,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+// on lesson hash password and register route
