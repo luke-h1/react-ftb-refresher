dadf79c30a3c605e5040cf132a362bd2b419ab78
luke-h1 <luke.howsam@yahoo.com>
2020-11-12 13:31:54 +0000

    hash pwds

--- project-2-contact-keeper/routes/users.js
+++ project-2-contact-keeper/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('express').Router();
 const User = require('../models/User');
 const { check, validationResult } = require('express-validator/check');
-
+const bcrypt = require('bcryptjs');
 /* 
 @route    POST    /api/users
 @desc     Register a user 
@@ -28,8 +28,18 @@ router.post(
       if (user) {
         return res.status(400).json({ msg: 'User already exists' });
       }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User saved to MongoDB');
     } catch (e) {
-      console.log(e);
+      console.log(e.message);
+      res.send(500).send('Server Error...')
     }
   }
 );
