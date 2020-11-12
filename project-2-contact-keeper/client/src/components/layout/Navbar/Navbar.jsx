663ee5c52ee142c1272bf8e9ec7a8adff0ee3dd1
luke-h1 <luke.howsam@yahoo.com>
2020-11-12 18:37:54 +0000

    finish navbar

--- project-2-contact-keeper/client/src/components/layout/Navbar/Navbar.jsx
+++ project-2-contact-keeper/client/src/components/layout/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AiTwotoneIdcard } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 const Navbar = ({ title }) => {
   return (
     <>
       <div className="navbar bg-primary">
-        <AiTwotoneIdcard style={{ fontSize: '70px' }} />
-        <h1>{title}</h1>
+        <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>
+          <AiTwotoneIdcard style={{ fontSize: '70px' }} />
+        </Link>
+        <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>
+          <h1>{title}</h1>
+        </Link>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
       </div>
     </>
   );
