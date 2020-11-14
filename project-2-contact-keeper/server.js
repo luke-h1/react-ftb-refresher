caa916f9c5abfdc2c1cff64f45e620f83e89e168
luke-h1 <luke.howsam@yahoo.com>
2020-11-14 17:53:08 +0000

    Prepare for deploy

--- project-2-contact-keeper/server.js
+++ project-2-contact-keeper/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-
+const path = require('path');
 
 // connect to mongodb
 connectDB();
@@ -9,14 +9,23 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the contact keeper API ! 📱 😀');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the contact keeper API ! 📱 😀');
+// });
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// SERVE STATIC ASSETS IN PROD
+if (process.env.NODE_ENV === 'production') {
+  // SET STATIC FOLDER
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
