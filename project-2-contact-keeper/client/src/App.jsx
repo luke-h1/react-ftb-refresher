cfcabe0b91cb6df022f4be8ca15109a3d1702dd4
luke-h1 <luke.howsam@yahoo.com>
2020-11-14 12:53:31 +0000

    login and register auth forms

--- project-2-contact-keeper/client/src/App.jsx
+++ project-2-contact-keeper/client/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './components/Pages/Home/Home';
 import About from './components/Pages/About/About';
 import ContactState from './components/context/Contact/ContactState';
 import AuthState from './components/context/auth/AuthState';
+import Register from './components/auth/Register/Register';
+import Login from './components/auth/Login/Login';
 function App() {
   return (
     <AuthState>
@@ -17,6 +19,8 @@ function App() {
               <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/about" exact component={About} />
+                <Route path="/register" exact component={Register} />
+                <Route path="/Login" exact component={Login} />
               </Switch>
             </div>
           </>
--- project-2-contact-keeper/client/src/components/context/auth/AuthState.jsx
+++ project-2-contact-keeper/client/src/components/context/auth/AuthState.jsx
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import AuthContext from './AuthContext';
+import AuthContext from './authContext';
 import AuthReducer from './AuthReducer';
 import {
   REGISTER_SUCCESS,
